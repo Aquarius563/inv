4c3902982744f6e026fb4baa274f16c88e6e31ae
Adhip <adhipjajoo563@gmail.com>
2024-06-12 10:44:55 +0530

    first commit

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set the base URL for Axios
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://inventory-iyas.onrender.com',
 });
 
 export default axiosInstance;
